368eab033b9614c59982a2eb4f1b9754356daf51
Dimasqi Ramadhani <dimasqir@gmail.com>
2021-06-30 06:30:06 +0800

    Update Kantin-FTI.py

--- Kantin-FTI.py
+++ Kantin-FTI.py
@@ -1,6 +1,8 @@
+# import library
 from os import system, __name__
 import time
 
+# fungsi validasi inputan user agar hanya berupa integer
 def inputangka(pesan):
     while True:
         try:
@@ -10,6 +12,7 @@ def inputangka(pesan):
         else:
             return pesan
 
+# fungsi validasi inputan user agar hanya berupa integer        
 def inputangka_mkn(pesan):
     while True:
         try:
@@ -19,6 +22,7 @@ def inputangka_mkn(pesan):
         else:
             return pesan
 
+# fungsi validasi inputan user agar hanya berupa integer
 def inputangka_mnm(pesan):
     while True:
         try:
@@ -28,6 +32,7 @@ def inputangka_mnm(pesan):
         else:
             return pesan
 
+# fungsi validasi inputan user agar hanya berupa integer
 def inputangka_bayar(pesan):
     while True:
         try:
@@ -37,6 +42,7 @@ def inputangka_bayar(pesan):
         else:
             return pesan
 
+# fungsi validasi inputan user agar pilihan makanan sesuai dengan yang ada list menu
 def inputmakanan(pesan):
     while(pesan != "a" and pesan != "b" and pesan != "c" and pesan != "d" and pesan != "e" and pesan != "f"):
         ulang = input(">> Masukkan Kode Makanan : ")
@@ -47,6 +53,7 @@ def inputmakanan(pesan):
             print("\nWARNING : Masukkan Inputan Sesuai Pilihan Menu\n")
             pass
 
+# fungsi validasi inputan user agar pilihan minuman sesuai dengan yang ada list menu
 def inputminuman(pesan):
      while(pesan != "a" and pesan != "b" and pesan != "c" and pesan != "d" and pesan != "e"):
         ulang = input(">> Masukkan Kode Minuman     : ")
@@ -57,6 +64,7 @@ def inputminuman(pesan):
             print("\nWARNING : Masukkan Inputan Sesuai Pilihan Menu\n")
             pass
 
+# fungsi validasi inputan user sesuai dengan yang diperintahkan [Y/T]
 def inputpilihan(pesan):
       while(pesan != "Y" and pesan != "y" and pesan != "T" and pesan != "t"):
         ulang = input(">> Transaksi Ulang [Y/T] : ")
@@ -67,6 +75,7 @@ def inputpilihan(pesan):
             print("\nWARNING : Masukkan Input Perintah Berupa [Y/T]!!\n")
             pass
 
+# fungsi validasi inputan user sesuai dengan yang diperintahkan [Y/T]        
 def inputpilihan_mkn(pesan):
       while(pesan != "Y" and pesan != "y" and pesan != "T" and pesan != "t"):
         ulang = input("\n>> Pilih Menu Makanan Kembali [Y/T]     : ")
@@ -77,6 +86,7 @@ def inputpilihan_mkn(pesan):
             print("\nWARNING : Masukkan Input Perintah Berupa [Y/T]!!")
             pass
 
+# fungsi validasi inputan user sesuai dengan yang diperintahkan [Y/T]        
 def inputpilihan_mnm(pesan):
       while(pesan != "Y" and pesan != "y" and pesan != "T" and pesan != "t"):
         ulang = input("\n>> Pilih Menu Minuman Kembali [Y/T]     : ")
@@ -87,6 +97,7 @@ def inputpilihan_mnm(pesan):
             print("\nWARNING : Masukkan Input Perintah Berupa [Y/T]!!")
             pass
 
+# fungsi validasi inputan user sesuai dengan yang diperintahkan [Y/T]
 def inputtambahan_mkn(pesan):
       while(pesan != "Y" and pesan != "y" and pesan != "T" and pesan != "t"):
         ulang = input("\n>> Ingin Menambahkan Menu Makanan [Y/T] : ")
@@ -96,7 +107,8 @@ def inputtambahan_mkn(pesan):
         else:
             print("\nWARNING : Masukkan Input Perintah Berupa [Y/T]!!")
             pass
-
+        
+# fungsi validasi inputan user sesuai dengan yang diperintahkan [Y/T]
 def inputtambahan_mnm(pesan):
       while(pesan != "Y" and pesan != "y" and pesan != "T" and pesan != "t"):
         ulang = input("\n>> Ingin Menambahkan Menu Minuman [Y/T] : ")
@@ -107,6 +119,7 @@ def inputtambahan_mnm(pesan):
             print("\nWARNING : Masukkan Input Perintah Berupa [Y/T]!!")
             pass
 
+# fungsi konversi integer menjadi format mata uang
 def uang(nilai):
     x = str(nilai)
 
@@ -117,6 +130,7 @@ def uang(nilai):
         b = x[-3:]
         return uang(a) + "." + b
 
+# mencetak resi transaski berupa file.txt dari suatu transaksi    
 def resi_transaksi():
 
     resi = open("Resi-Transaksi.txt","w")
@@ -168,7 +182,8 @@ def resi_transaksi():
     resi.write("-> Pembayaran     : " + uang(bayar) + "\n")
     resi.write("-> Kembalian      : " + uang(kembalian) + "\n")
     resi.write("\n+============ TERIMA KASIH ===========+\n")
-    
+
+# fungsi menampilkan list makanan
 def daftar_makanan():
     
     print()
@@ -187,6 +202,8 @@ def daftar_makanan():
     print("+==============================================+")
     print()
 
+
+# fungsi menampilkan list minuman    
 def daftar_minuman():
 
     print()
@@ -204,6 +221,7 @@ def daftar_minuman():
     print("+==============================================+")
     print()
 
+# program utama dari transaksi kantin FTI
 while True:
 
     _ = system('cls')
@@ -220,7 +238,8 @@ while True:
     print("|----------------------------------------------|")
     print("+==============================================+")
     print()
-
+    
+    # variabel list makanan
     makanan        = ["Nasi Goreng","Lontong Goreng","Bakso Goreng","Rujak Goreng","Rujak Bakso Pecel"]
     harga_mkn      = [15000,14900,12900,13000,15000,17000]
     kode_mkn       = ["a","b","c","d","e","f"]
@@ -231,7 +250,8 @@ while True:
     list_kode_mkn  = []
     qty_mkn        = 0
     mkn            = False
-
+    
+    # variabel list minuman
     minuman        = ["Teh Dingin/Panas","Kopi Dingin","Kopi Teh Panas","Coca-Cola Dingin","Coca-Cola Panas"]
     harga_mnm      = [2500,5000,6500,3500,5000]
     kode_mnm       = ["a","b","c","d","e"]
@@ -243,9 +263,10 @@ while True:
     qty_mnm        = 0
     mnm            = False 
 
-
+    # inputan user untuk memilih antara menu makanan atau minuman
     ipt_menu = inputangka(">> Masukkan No Daftar Menu : ")
-
+    
+    # perulangan yang digunakan untuk memvalidasi inputan agar hanya berupa pilihan 1 atau 2
     while(ipt_menu <= 0 or ipt_menu >= 3):
         print("\nWARNING!! : Masukkan Inputan Sesuai Pilihan Menu\n")
         ipt_menu = inputangka(">> Masukkan No Daftar Menu : ")
@@ -256,18 +277,22 @@ while True:
             pass
 
     _ = system("cls")
-
+    
+    # penkondisian utama dari pilihan user 1. untuk makanan
     if(ipt_menu == 1):
-
+        
+        # perulangan pertama untuk memilih menu makanan
         mkn = True
         urutan = 1
         i_mkn = 0
         sts = True
         while (sts == True):
-
+            
+            # inputan user agar memasukkan kode makanan
             daftar_makanan()
             ipt_makanan  = inputmakanan(">> Masukkan Kode Makanan : ")
-
+            
+            # perulangan agar user tidak memasukkan kode makanan yang sama ke dalam tempt storage
             x = ipt_makanan in list_kode_mkn
             while(x == True):
                 print("\nWARNING!! : Makanan Tersebut Telah Terdaftar Di List Anda, Silahkan Pilih Makanan yang Lain\n")
@@ -278,9 +303,11 @@ while True:
                     break
                 else:
                     continue
-
+            
+            # inputan user untuk memasukkan kuantiti dari makanan yang dipesan
             qty_mkn = inputangka_mkn(">> Masukkan Jumlah Porsi : ")
             
+            # validasi dari inputan user agar tidak memasukkan kuantiti <= 0
             while(qty_mkn <= 0):
                 print("\nWARNING!! : Masukkan Inputan Harus >= 0\n")
                 qty_mkn = inputangka_mkn(">> Masukkan Jumlah Porsi : ")
@@ -289,7 +316,8 @@ while True:
                     break
                 else:
                     pass
-
+            
+            # variabel yang digunakan sebagai temp storage, untuk menyimpan semua pesanan yang dipilih
             no_index = kode_mkn.index(ipt_makanan)
             list_makanan.append(makanan[no_index])
             list_harga_mkn.append(harga_mkn[no_index])
@@ -309,7 +337,12 @@ while True:
             i_mkn += 1
             i_mnm = 0
             urutan += 1
-
+            
+            # inputan untuk memastikan jika user ingin memesan makanan lebih dari sekali
+            # jika user menjawab Y atau y, maka sistem akan balik looping untuk menampilkan menu makanan
+            # jika T atau t, maka sistem akan menawarkan pilihan kembali apakah ingin menambahkan minuman
+            # jika Y atau y, maka sistem akan beralih ke menu daftar minuman
+            # jika T atau t, maka sistem akan langsung keluar dari looping dan melanjutkan untuk melakukan pembayaran
             pilihan_mkn = inputpilihan_mkn("\n>> Pilih Menu Makanan Kembali [Y/T]     : ")
             if(pilihan_mkn == "Y" or pilihan_mkn == "y"):
                 sts = True
@@ -319,16 +352,19 @@ while True:
                 pilihan_mnm = inputtambahan_mnm("\n>> Ingin Menambahkan Menu Minuman [Y/T] : ")
                 if(pilihan_mnm == "Y" or pilihan_mnm == "y"):
                     _ = system('cls')
-
+                    
+                    # perulangan kedua jika user ingin menambahkan menu minuman
                     mnm = True
                     urutan = 1
                     i_mnm = 0
                     sts = True
                     while(sts == True):
-
+                        
+                        # inputan user agar memasukkan kode minuman
                         daftar_minuman()
                         ipt_minuman  = inputminuman(">> Masukkan Kode Minuman     : ")
-
+                        
+                        # perulangan agar user tidak memasukkan daftar menu yang sama ke dalam list
                         x = ipt_minuman in list_kode_mnm
                         while(x == True):
                             print("\nWARNING!! : Minuman Tersebut Telah Terdaftar Di List Anda, Silahkan Pilih Minuman yang Lain\n")
@@ -339,9 +375,11 @@ while True:
                                 break
                             else:
                                 continue
-
+                                
+                        # inputan user untuk memasukkan kuantiti dari menu yang dipilih
                         qty_mnm = inputangka_mnm(">> Masukkan Jumlah Kuantitas : ")
-
+                        
+                        # perulangan agar user tidak memasukkan kuantiti <= 0
                         while(qty_mnm <= 0):
                             print("\nWARNING!! : Masukkan Inputan Harus >= 0\n")
                             qty_mnm = inputangka_mnm(">> Masukkan Jumlah Kuantitas : ")
@@ -350,7 +388,8 @@ while True:
                                 break
                             else:
                                 pass
-
+                        
+                        # variabel yang berfungsi sebagai temp storage, dari pilihan menu yang dipilih user
                         no_index = kode_mnm.index(ipt_minuman)
                         list_minuman.append(minuman[no_index])
                         list_harga_mnm.append(harga_mnm[no_index])
@@ -369,7 +408,8 @@ while True:
                         list_qty_mnm.append(qty_mnm)
                         i_mnm += 1
                         urutan += 1
-
+                        
+                        # inputan berupa pilihan, apakah user ingin memilih menu minumab yang lain atau tidak
                         pilihan_mnm = inputpilihan_mnm("\n>> Pilih Menu Minuman Kembali [Y/T]     : ")
 
                         if(pilihan_mnm == "Y" or pilihan_mnm == "y"):
@@ -381,17 +421,21 @@ while True:
                 else:
                     break        
 
+    # pengkondisian kedua jika user pada tahap awal langsung memilih menu minuman
     else:
-
+        
+        # perulangan pertama untuk memilih menu minuman
         mnm = True
         urutan = 1
         i_mnm = 0
         sts = True
         while(sts == True):
-
+            
+            # inputan user agar dapat memasukkan kode minuman
             daftar_minuman()
             ipt_minuman  = inputminuman(">> Masukkan Kode Minuman     : ")
-
+            
+            # perulangan agar user tidak memasukkan kode yang sama ke dalam list pesanan mereka
             x = ipt_minuman in list_kode_mnm
             while(x == True):
                 print("\nWARNING!! : Minuman Tersebut Telah Terdaftar Di List Anda, Silahkan Pilih Minuman yang Lain\n")
@@ -402,9 +446,11 @@ while True:
                     break
                 else:
                     continue
-
+            
+            # inputan mengenai kuantiti yang ingin dipesan
             qty_mnm = inputangka_mnm(">> Masukkan Jumlah Kuantitas : ")
             
+            # perulangan agar user tidak memasukkan kuantiti <= 0
             while(qty_mnm <= 0):
                 print("\nWARNING!! : Masukkan Inputan Harus >= 0\n")
                 qty_mnm = inputangka_mnm(">> Masukkan Jumlah Kuantitas : ")
@@ -412,8 +458,9 @@ while True:
                 if(qty_mnm > 0):
                     break
                 else:
-                    pass
-
+                    pass    
+                
+            # variabel yang berfungsi sebagai temp storage, untuk menyimpan semua pilihan menu yang dipesan
             no_index = kode_mnm.index(ipt_minuman)
             list_minuman.append(minuman[no_index])
             list_harga_mnm.append(harga_mnm[no_index])
@@ -433,7 +480,12 @@ while True:
             i_mnm += 1
             i_mkn = 0
             urutan += 1
-
+            
+            # inputan untuk memastikan jika user ingin memesan makanan lebih dari sekali
+            # jika user menjawab Y atau y, maka sistem akan balik looping untuk menampilkan menu minuman
+            # jika T atau t, maka sistem akan akan menawarkan pilihan kembali apakah ingin menambahkan makanan
+            # jika Y atau y, maka sistem akan beralih ke menu daftar makanan
+            # jika T atau t, maka sistem akan langsung keluar dari looping dan melanjutkan untuk melakukan pembayaran
             pilihan_mnm = inputpilihan_mnm("\n>> Pilih Menu Minuman Kembali [Y/T]     : ")
 
             if(pilihan_mnm == "Y" or pilihan_mnm == "y"):
@@ -444,16 +496,19 @@ while True:
                 pilihan_mkn = inputtambahan_mkn("\n>> Ingin Menambahkan Menu Makanan [Y/T] : ")
                 if(pilihan_mkn == "Y" or pilihan_mkn == "y"):
                     _ = system('cls')
-
+                    
+                    # perulangan kedua agar user dapat menambahkan menu makanan kedalam list mereka
                     mkn = True                    
                     urutan = 1
                     i_mkn = 0
                     sts = True
                     while(sts == True):
-
+                        
+                        # inputan user agar dapat menginputkan kode makanan yang ingin dipesan
                         daftar_makanan()
                         ipt_makanan  = inputmakanan(">> Masukkan Kode Makanan : ")
-
+                        
+                        # perulangan agar user tidak memasukkan kode yang sama kedalam list mereka
                         x = ipt_makanan in list_kode_mkn
                         while(x == True):
                             print("\nWARNING!! : Makanan Tersebut Telah Terdaftar Di List Anda, Silahkan Pilih Makanan yang Lain\n")
@@ -464,9 +519,11 @@ while True:
                                 break
                             else:
                                 continue
-
+                        
+                        # inputan agar user dapat memasukkan kuantiti dari pesanan yang dipilih
                         qty_mkn = inputangka_mkn(">> Masukkan Jumlah Porsi : ")
-
+                        
+                        # inputan user agar tidak memasukkan kuantiti <= 0
                         while(qty_mkn <= 0):
                             print("\nWARNING!! : Masukkan Inputan Harus >= 0\n")
                             qty_mkn= inputangka_mkn(">> Masukkan Jumlah Porsi : ")
@@ -475,7 +532,8 @@ while True:
                                 break
                             else:
                                 pass
-
+                        
+                        # variabel yang berfungsi sebagai temp storage, untuk menyimpan data menu yang dipesan user
                         no_index = kode_mkn.index(ipt_makanan)
                         list_makanan.append(makanan[no_index])
                         list_harga_mkn.append(harga_mkn[no_index])
@@ -494,7 +552,8 @@ while True:
                         list_qty_mkn.append(qty_mkn)
                         i_mkn += 1
                         urutan += 1
-
+                        
+                        # inputan user agar dapat memilih kembali menu makanan
                         pilihan_mkn = inputpilihan_mkn("\n>> Pilih Menu Makanan Kembali [Y/T]     : ")
 
                         if(pilihan_mkn == "Y" or pilihan_mkn == "y"):
@@ -507,7 +566,8 @@ while True:
                     break
     
     _ =  system('cls')
-
+    
+    # menampilkan list data pemesanan dari temp storage
     time.sleep(0.7)
     if(mkn == True):
         urutan = 1
@@ -546,7 +606,8 @@ while True:
             list_total_mnm[i]
             urutan += 1
             i += 1
-
+            
+    # proses transaksi
     tot_mkn = sum(list_total_mkn)
     tot_mnm = sum(list_total_mnm)
     tot     = tot_mkn + tot_mnm 
@@ -587,7 +648,7 @@ while True:
     print("-> Jumlah Makanan :",sum(list_qty_mkn),"Makanan")
     print("-> Jumlah Minuman :",sum(list_qty_mnm),"Minuman")
     print("---------------------------------------\n")
-
+    
     if(mkn == True):
         urutan = 1
         i = 0 
@@ -631,9 +692,10 @@ while True:
     print("-> Kembalian      :",uang(kembalian))
     print("\n+============ TERIMA KASIH ===========+\n")
 
-    # print resi transaksi pembayaran
+    # cetak resi transaksi pembayaran berupa file.txt
     resi_transaksi() 
-
+    
+    # inputan jika user ingin mengulang program
     ulang = inputpilihan(">> Transaksi Ulang [Y/T] : ")
 
     if(ulang == "Y" or ulang == "y"):
@@ -641,6 +703,7 @@ while True:
     else:
         break
 
+# akhir program
 print("\n+======= PROGRAM TELAH BERAKHIR ======+")
 
 
